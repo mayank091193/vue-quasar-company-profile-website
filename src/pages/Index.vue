28d7714496f539981aff5626e780022e57bb484c
Mayank Patel <mayank091193@gmail.com>
2020-05-02 16:43:58 +0530

    Initial commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,462 @@
+<template>
+  <q-layout>
+    <q-header elevated style="background-color: #1c1b21">
+      <q-toolbar>
+        <q-toolbar-title :style="'color:'+theme_color">
+          Quasar App
+        </q-toolbar-title>
+
+        <div>
+          <q-btn :style="'background:'+ theme_color +'; color: white'" class="q-ma-xs" dense icon="color_lens">
+            <q-tooltip transition-show="flip-right"
+                       transition-hide="flip-left" self="center left" anchor="center right">Theme color
+            </q-tooltip>
+            <q-menu anchor="bottom left" self="bottom right" transition-show="flip-right"
+                    transition-hide="flip-left">
+              <q-color
+                v-model="bg_color"
+                no-header
+                no-footer
+                default-view="palette"
+                class="my-picker"
+                @input="changeBgColor()"
+              />
+            </q-menu>
+          </q-btn>
+        </div>
+      </q-toolbar>
+    </q-header>
+    <q-page-container>
+      <q-page>
+        <q-carousel
+          animated
+          v-model="slide"
+          arrows
+          swipeable
+          navigation
+          infinite
+          autoplay
+          control-color="white"
+          transition-prev="slide-right"
+          transition-next="slide-left"
+          height="693px"
+        >
+          <q-carousel-slide :name="1" img-src="/statics/images/image_1.jpg">
+            <div class="absolute-bottom custom-caption">
+              <div class="text-h2">First stop</div>
+              <div class="text-subtitle1">Mountains</div>
+            </div>
+          </q-carousel-slide>
+          <q-carousel-slide :name="2" img-src="/statics/images/image_2.jpg">
+            <div class="absolute-bottom custom-caption">
+              <div class="text-h2">Second stop</div>
+              <div class="text-subtitle1">Famous City</div>
+            </div>
+          </q-carousel-slide>
+          <q-carousel-slide :name="3" img-src="/statics/images/image_3.jpg">
+            <div class="absolute-bottom custom-caption">
+              <div class="text-h2">Third stop</div>
+              <div class="text-subtitle1">Famous Bridge</div>
+            </div>
+          </q-carousel-slide>
+        </q-carousel>
+        <div class="row">
+          <div class="col-md-3 col-lg-3 col-xs-12 col-sm-12">
+            <q-card class="q-pa-lg flex flex-center text-center box-shadow" style="margin: 40px;">
+
+
+              <q-card-section>
+                <q-icon size="110px" name="ballot" :style="'color:'+theme_color"/>
+                <div class="text-h6">Web &nbsp;Design</div>
+
+              </q-card-section>
+
+              <q-card-section class="q-pt-none">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt.
+              </q-card-section>
+            </q-card>
+          </div>
+          <div class="col-md-3 col-lg-3 col-xs-12 col-sm-12">
+            <q-card class="q-pa-lg flex flex-center text-center box-shadow" style="margin: 40px;">
+
+
+              <q-card-section>
+                <q-icon size="110px" name="view_comfy" :style="'color:'+theme_color"/>
+                <div class="text-h6">Graphics Design</div>
+
+              </q-card-section>
+
+              <q-card-section class="q-pt-none">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt.
+              </q-card-section>
+            </q-card>
+          </div>
+          <div class="col-md-3 col-lg-3 col-xs-12 col-sm-12">
+            <q-card class="q-pa-lg flex flex-center text-center box-shadow" style="margin: 40px;">
+
+
+              <q-card-section>
+                <q-icon size="110px" name="people" :style="'color:'+theme_color"/>
+                <div class="text-h6">Wordpress Theme</div>
+
+              </q-card-section>
+
+              <q-card-section class="q-pt-none">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt.
+              </q-card-section>
+            </q-card>
+          </div>
+
+          <div class="col-md-3 col-lg-3 col-xs-12 col-sm-12">
+            <q-card class="q-pa-lg flex flex-center text-center box-shadow" style="margin: 40px;">
+
+
+              <q-card-section>
+                <q-icon size="110px" name="people" :style="'color:'+theme_color"/>
+                <div class="text-h6">Wordpress Plugin</div>
+
+              </q-card-section>
+
+              <q-card-section class="q-pt-none">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt.
+              </q-card-section>
+            </q-card>
+          </div>
+        </div>
+        <q-parallax :speed="0.5">
+
+          <template v-slot:media>
+            <img src="/statics/images/parallax.jpg">
+          </template>
+          <div class="full-width full-height flex flex-center" style="background-color: #000000ad !important;">
+            <h3 class="text-white text-center">Lorem ipsum dolor sit amet, consectetur <br>adipiscing elit, sed do
+              eiusmod tempor incididunt ut <br> labore et dolore magna aliqua.
+              <br>
+              <br>
+              <q-btn type="a" href="https://github.com/sponsors/mayank091193" size="lg"
+                     :style="'background:'+ theme_color +'; color: white'" label="Support Me"/>
+            </h3>
+          </div>
+        </q-parallax>
+        <div style="background: #f7f7f7">
+          <br>
+          <div class="row">
+            <div class="col-md-12 col-lg-12 col-xs-12 col-sm-12">
+              <h5 class="text-center">Our Portfolio
+                <br>
+                <span class="text-center text-grey text-h6">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.</span>
+              </h5>
+            </div>
+          </div>
+          <br>
+          <div class="row text-center" style="padding-bottom: 99px">
+            <div class="col-md-12 col-sm-12 col-xs-12 col-lg-12">
+              <div style="line-height:0">
+                <span v-for="index in 6">
+                  <q-flashcard :no-hover="hover" :style="style">
+                    <q-flashcard-section transition="nudge-in" :active="active">
+                      <img :src="'/statics/images/'+index+'.jpg'" width=340 height=263>
+                    </q-flashcard-section>
+                    <q-flashcard-section transition="fade-in" class="fit" :active="active">
+                      <div class="fit" :style="background_style"></div>
+                      <q-flashcard-section transition="drop-down" class="text-center my-header" :active="active">
+                        Profile Title
+                      </q-flashcard-section>
+                      <q-flashcard-section transition="slide-up-in" class="my-text" :active="active">
+                        For beautiful eyes, look for the good in others; for beautiful lips, speak only words of kindness; and for poise, walk with the knowledge that you are never alone.
+                      </q-flashcard-section>
+                      <q-flashcard-section transition="fade-in" class="fit flex justify-center items-end q-pb-lg"
+                                           :active="active">
+                        <q-btn class="q-mr-md" :style="'color:'+theme_color" style="background-color:white" round
+                               icon="card_giftcard"/>
+                        <q-btn :style="'color:'+theme_color" style="background-color:white" round icon="link"/>
+                      </q-flashcard-section>
+                    </q-flashcard-section>
+                  </q-flashcard>
+                  <br v-if="index==3"/>
+                  </span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12 col-lg-12 col-xs-12 col-sm-12">
+            <h5 class="text-center">About Us
+              <br>
+              <span
+                class="text-center text-grey text-h6">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
+            </h5>
+          </div>
+        </div>
+        <div class="row text-center flex flex-center">
+          <div class="col-md-12 col-lg-12 col-sx-12 col-sm-12 q-gutter-md flex flex-center">
+            <div class="col-md-4 col-lg-4 col-sx-12 col-sm-12">
+              <q-card @mouseover="hoverOver(1)" @mouseout="hoverOutTimeout(1)" style="border: none;" class="my-card text-center box-shadow" flat bordered>
+                <q-img
+                  src="/statics/images/about_1.jpg"
+                />
+
+                <q-card-section>
+                  <div class="text-overline text-orange-9">Overline</div>
+                  <div :class="about_heading_1" :style="'color: ' + about_heading_color_1" class="about_heading_1 text-h6 q-mt-sm q-mb-xs">OFFICE PHILOSOPHY</div>
+                  <div class="text-caption text-grey-9">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  </div>
+                </q-card-section>
+              </q-card>
+            </div>
+            <div class="col-md-4 col-lg-4 col-sx-12 col-sm-12">
+              <q-card @mouseover="hoverOver(2)" @mouseout="hoverOutTimeout(2)" :style="'color: ' + about_heading_color_2" style="border: none;" class="my-card text-center box-shadow" flat bordered>
+                <q-img
+                  src="/statics/images/about_2.jpg"
+                />
+
+                <q-card-section>
+                  <div class="text-overline text-orange-9">Overline</div>
+                  <div :class="about_heading_2" :style="'color: ' + about_heading_color_2" class="text-h6 q-mt-sm q-mb-xs">OFFICE MISSION</div>
+                  <div class="text-caption text-grey-9">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  </div>
+                </q-card-section>
+              </q-card>
+            </div>
+            <div class="col-md-4 col-lg-4 col-sx-12 col-sm-12">
+              <q-card @mouseover="hoverOver(3)" @mouseout="hoverOutTimeout(3)" style="border: none;" class="my-card text-center box-shadow" flat bordered>
+                <q-img
+                  src="/statics/images/about_3.jpg"
+                />
+
+                <q-card-section>
+                  <div class="text-overline text-orange-9">Overline</div>
+                  <div :class="about_heading_3" :style="'color: ' + about_heading_color_3" class="text-h6 q-mt-sm q-mb-xs">OFFICE VALUE & RULES</div>
+                  <div class="text-caption text-grey-9">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua.
+                  </div>
+                </q-card-section>
+              </q-card>
+            </div>
+          </div>
+        </div>
+        <br>
+        <br>
+        <div style="background: #f7f7f7">
+          <div class="row">
+            <div class="col-md-12 col-lg-12 col-xs-12 col-sm-12">
+              <h5 class="text-center">Our Team
+                <br>
+                <span
+                  class="text-center text-grey text-h6">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
+              </h5>
+            </div>
+          </div>        
+          <div class="row text-center flex flex-center">
+            <div class="col-md-12 col-lg-12 col-sx-12 col-sm-12 q-px-xl">
+              <q-carousel
+              v-model="team_slide"
+              transition-prev="slide-right"
+              transition-next="slide-left"
+              swipeable
+              animated
+              control-color="primary"
+              navigation
+              padding
+              height="300px"
+              class="rounded-borders"
+            >
+              <q-carousel-slide v-for="val in [1,2,3]" :name="val" style="background: #f7f7f7" class="column no-wrap">
+                <div class="row fit justify-center items-center q-gutter-xs q-col-gutter no-wrap">
+                  <span v-for="index in 4">
+                  <q-flashcard :no-hover="hover" :style="team_style">
+                    <q-flashcard-section transition="['nudge-out', 'fade-out']" :active="active">
+                      <img :src="'/statics/images/team_'+index+'.jpg'" width=260 height=263>
+                    </q-flashcard-section>
+                    <q-flashcard-section transition="fade-in" class="fit" :active="active">
+                      <div class="fit" :style="background_style"></div>
+                      <q-flashcard-section transition="shake-down" class="text-center team-header" :active="active">
+                        JOHN DOE
+                      </q-flashcard-section>
+                      <q-flashcard-section transition="spin-in" class="team-text" :active="active">
+                        Founder and Director
+                      </q-flashcard-section>
+                      <q-flashcard-section style="padding-bottom: 50px" transition="slide-up-in" class="fit flex justify-center items-end"
+                                           :active="active">
+                        <q-btn class="q-mr-md" :style="'color:'+theme_color" style="background-color:white" round
+                               icon="card_giftcard"/>
+                        <q-btn :style="'color:'+theme_color" style="background-color:white" round icon="link"/>
+                      </q-flashcard-section>
+                    </q-flashcard-section>
+                  </q-flashcard>
+                  </span>
+                </div>
+              </q-carousel-slide>
+            </q-carousel>
+            </div>
+          </div>
+        </div>
+      </q-page>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<style src="@quasar/quasar-ui-qflashcard/dist/index.css"></style>
+
+<script>
+    import {QFlashcard, QFlashcardSection} from '@quasar/quasar-ui-qflashcard'
+
+    export default {
+        components: {
+            QFlashcard,
+            QFlashcardSection
+        },
+        data() {
+            return {
+                slide: 1,
+                team_slide: 1,
+                bg_color: 'rgb(0, 123, 245)',
+                hover: false,
+                active: false,
+                theme_color: 'rgb(0, 123, 245)',
+                background_style: 'background-color:rgba(0, 123, 245, 0.7);',
+                about_heading_1: [],
+                about_heading_2: [],
+                about_heading_3: [],
+                about_heading_color_1: '#424242',
+                about_heading_color_2: '#424242',
+                about_heading_color_3: '#424242'
+            }
+        },
+        computed: {
+            style() {
+                return {
+                    width: '340px',
+                    height: '263px',
+                    backgroundImage: `url('https://raw.githubusercontent.com/quasarframework/quasar-ui-qflashcard/dev/demo/src/statics/bgimg.jpg')`, // eslint-disable-line
+                    textAlign: 'center',
+                    boxShadow: '1px 1px 2px #e6e6e6',
+                    display: 'inline-block'
+                }
+            },
+            team_style() {
+                return {
+                    width: '260px',
+                    height: '263px',
+                    backgroundImage: `url('https://raw.githubusercontent.com/quasarframework/quasar-ui-qflashcard/dev/demo/src/statics/bgimg.jpg')`, // eslint-disable-line
+                    textAlign: 'center',
+                    boxShadow: '1px 1px 2px #e6e6e6',
+                    display: 'inline-block'
+                }
+            }
+        },
+        methods: {
+            changeBgColor() {
+                this.theme_color = this.bg_color;
+                this.background_style = 'background-color:' + this.bg_color.split(')')[0] + ',0.7)';
+            },
+            hoverOver: function(index) {
+              this['about_heading_color_' + index] = this.theme_color;
+              this['about_heading_' + index] = ['animated', 'bounceIn'];              
+            },
+            hoverOutTimeout: function(index) {
+              this['about_heading_color_' + index] = '#424242';
+              setTimeout(() => {
+                this['about_heading_' + index] = [];                
+              }, 1000);              
+            },
+        }
+    }
+</script>
+
+<style lang="sass" scoped>
+    .custom-caption
+      text-align: center
+      padding: 12px
+      color: white
+      background-color: rgba(0, 0, 0, .3)
+
+    .description
+      padding: 10px
+      background-color: black
+      color: white
+      box-shadow: 1px 1px 2px #e6e6e6
+
+    .my-header
+      width: 200px
+      top: 0
+      height: 45px
+      color: black
+      background-color: rgba(255,255,255, 0.8)
+      text-transform: uppercase
+      text-align: center
+      font-size: 17px
+      margin: 20px 0 0 68px
+      padding: 25px
+
+    .my-text
+      width: 100%
+      top: 0
+      height: 90px
+      color: white
+      text-align: center
+      font-size: 15px
+      margin: 79px 0 0 0
+      padding: 20px
+      line-height: normal
+      font-family: Georgia, serif
+      font-style: italic
+
+    .my-button-container
+      width: 100%
+
+    .my-button
+      text-decoration: none
+      text-transform: uppercase
+      margin: 0 0 20px 0
+      text-align: center
+      padding: 7px 14px
+      background-color: #000
+      color: #fff
+      text-transform: uppercase
+      box-shadow: 0 0 1px #000
+      transition-delay: 0.2s
+
+    .my-button:hover
+      box-shadow: 0 0 5px #000
+
+    .box-shadow:hover
+      box-shadow: 0 10px 13px -6px rgba(0, 0, 0, 0.2), 0 20px 31px 3px rgba(0, 0, 0, 0.14), 0 8px 38px 7px rgba(0, 0, 0, 0.12) !important;
+
+    .my-card
+      width: 350px
+      max-width: 350px
+      margin-top: 10px
+
+    .team-header
+      width: 100%
+      top: 0
+      height: 45px
+      color: white
+      text-transform: uppercase
+      text-align: center
+      font-size: 17px
+      margin: 60px 0 0 0
+      padding: 12px
+
+    .team-text
+      width: 100%
+      top: 0
+      height: 90px
+      color: white
+      text-align: center
+      font-size: 15px
+      margin: 100px 0 0 0
+      padding: 20px
+      line-height: normal
+      font-family: Georgia, serif
+      font-style: italic
+</style>
+
