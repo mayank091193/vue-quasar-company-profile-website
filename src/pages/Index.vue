de4fbae24c841f9c7ecd2fdf5caa4e179d31c3c3
Mayank Patel <mayank091193@gmail.com>
2020-05-06 17:35:47 +0530

    Added pricing section.

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -253,7 +253,7 @@
           <div class="q-pa-xl">       
             <div class="row q-col-gutter-sm ">
               <div class="col-md-4 col-lg-4 col-sm-12 col-xs-12">
-                <q-item class="q-pa-none q-ml-xs ">
+                <q-item class="box-shadow q-pa-none q-ml-xs ">
                   <q-item-section side :style="'background-color:' + theme_color" class="q-pa-sm q-mr-none text-white">
                     <q-icon name="compare_arrows" size="65px"></q-icon>
                   </q-item-section>
@@ -264,7 +264,7 @@
                 </q-item>
               </div>
               <div class="col-md-4 col-lg-4 col-sm-12 col-xs-12">
-                <q-item class="q-pa-none q-ml-xs">
+                <q-item class="box-shadow q-pa-none q-ml-xs">
                   <q-item-section side :style="'background-color:' + theme_color" class="q-pa-sm q-mr-none text-white">
                     <q-icon name="web" size="65px"></q-icon>
                   </q-item-section>
@@ -275,7 +275,7 @@
                 </q-item>
               </div>
               <div class="col-md-4 col-lg-4 col-sm-12 col-xs-12">
-                <q-item class="q-pa-none q-ml-xs q-mr-xs">
+                <q-item class="box-shadow q-pa-none q-ml-xs q-mr-xs">
                   <q-item-section side :style="'background-color:' + theme_color" class="q-pa-sm q-mr-none text-white">
                     <q-icon name="fab fa-wordpress" size="65px"></q-icon>
                   </q-item-section>
@@ -288,7 +288,7 @@
             </div>
             <div class="row q-col-gutter-sm q-mt-lg">
               <div class="col-md-4 col-lg-4 col-sm-12 col-xs-12">
-                <q-item class="q-pa-none q-ml-xs ">
+                <q-item class="box-shadow q-pa-none q-ml-xs ">
                   <q-item-section side :style="'background-color:' + theme_color" class="q-pa-sm q-mr-none text-white">
                     <q-icon name="settings" size="65px"></q-icon>
                   </q-item-section>
@@ -299,7 +299,7 @@
                 </q-item>
               </div>
               <div class="col-md-4 col-lg-4 col-sm-12 col-xs-12">
-                <q-item class="q-pa-none q-ml-xs">
+                <q-item class="box-shadow q-pa-none q-ml-xs">
                   <q-item-section side :style="'background-color:' + theme_color" class="q-pa-sm q-mr-none text-white">
                     <q-icon name="fas fa-database" size="65px"></q-icon>
                   </q-item-section>
@@ -310,7 +310,7 @@
                 </q-item>
               </div>
               <div class="col-md-4 col-lg-4 col-sm-12 col-xs-12">
-                <q-item class="q-pa-none q-ml-xs q-mr-xs">
+                <q-item class="box-shadow q-pa-none q-ml-xs q-mr-xs">
                   <q-item-section side :style="'background-color:' + theme_color" class="q-pa-sm q-mr-none text-white">
                     <q-icon name="dynamic_feed" size="65px"></q-icon>
                   </q-item-section>
@@ -340,6 +340,7 @@
               transition-next="slide-left"
               swipeable
               animated
+	      infinite
               autoplay
               control-color="primary"
               navigation
@@ -347,7 +348,7 @@
               height="300px"
               class="rounded-borders"
             >
-              <q-carousel-slide v-for="val in [1,2,3]" :key="val" :name="val" style="background: #f7f7f7" class="column no-wrap">
+              <q-carousel-slide v-for="val in [1,2,3]" :key="val" :name="val" class="column no-wrap">
                 <div class="row fit justify-center items-center q-gutter-xs q-col-gutter no-wrap">
                   <span v-for="index in 4" :key="index">
                   <q-flashcard :no-hover="hover" :style="team_style">
@@ -375,7 +376,179 @@
               </q-carousel-slide>
             </q-carousel>
             </div>
-          </div>       
+          </div>          
+          <div class="pricing">
+          <div class="" style="background-color: #000000ad !important;">
+          <div class="row">
+            <div class="col-md-12 col-lg-12 col-xs-12 col-sm-12">
+              <h5 class="text-center text-white">Our Pricing Plan
+                <br>
+                <span
+                  class="text-center text-grey text-h6">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
+              </h5>
+            </div>
+          </div> 
+          <div class="row q-col-gutter-sm q-px-xl">
+          <div class="col-lg-3 col-md-3 col-xs-12 col-sm-6 q-px-lg">
+            <q-card>
+              <q-card-section class="text-white" style="background: #ec982f">
+                <div class="text-h6 text-center">
+                  FREE
+                </div>
+              </q-card-section>
+              <q-card-section class="q-pa-md">
+                <q-list dense padding style="border-bottom: 1px solid lightgrey;">
+                  <q-item><span class="text-weight-bold">For small size companies</span></q-item>
+                  <q-item>
+                      50 GB storage
+                  </q-item>
+                  <q-item>
+                      10 Domains
+                  </q-item>
+                  <q-item>
+                      15 Projects
+                  </q-item>
+                  <q-item>
+                      Free Deployment
+                  </q-item>
+                </q-list>
+              </q-card-section>
+              <q-card-section class="q-pa-none" style="color: #ec982f">
+                <div class="text-center">
+                  <span class="text-h2 text-center">$0</span>/mo
+                </div>
+              </q-card-section>
+              <q-card-section>
+                <div class="text-center q-px-lg">
+                  <q-btn size="md" class="full-width q-pa-xs"
+                     style="background: #ec982f; color: white" label="Choose Plan"/>
+                     <br>
+                     <div class="q-mt-md">Terms and Conditions apply</div>
+                </div>
+              </q-card-section>
+            </q-card>
+          </div>
+          <div class="col-lg-3 col-md-3 col-xs-12 col-sm-6 q-px-lg">
+            <q-card>
+              <q-card-section class="text-white" style="background: #f4655f">
+                <div class="text-h6 text-center">
+                  STANDARD
+                </div>
+              </q-card-section>
+              <q-card-section class="q-pa-md">
+                <q-list dense padding style="border-bottom: 1px solid lightgrey;">
+                  <q-item><span class="text-weight-bold">For medium companies</span></q-item>
+                  <q-item>
+                      50 GB storage
+                  </q-item>
+                  <q-item>
+                      10 Domains
+                  </q-item>
+                  <q-item>
+                      15 Projects
+                  </q-item>
+                  <q-item>
+                      Free Deployment
+                  </q-item>
+                </q-list>
+              </q-card-section>
+              <q-card-section class="q-pa-none" style="color: #f4655f">
+                <div class="text-center">
+                  <span class="text-h2 text-center">$50</span>/mo
+                </div>
+              </q-card-section>
+              <q-card-section>
+                <div class="text-center q-px-lg">
+                  <q-btn size="md" class="full-width q-pa-xs"
+                     style="background: #f4655f; color: white" label="Choose Plan"/>
+                     <br>
+                     <div class="q-mt-md">Terms and Conditions apply</div>
+                </div>
+              </q-card-section>
+            </q-card>
+          </div>
+          <div class="col-lg-3 col-md-3 col-xs-12 col-sm-6 q-px-lg">
+            <q-card>
+              <q-card-section class="text-white q-pa-md" style="background: #8b3bbd">
+                <div class="text-h6 text-center">
+                  PREMIUM
+                </div>
+              </q-card-section>
+              <q-card-section class="q-pa-md">
+                <q-list dense padding style="border-bottom: 1px solid lightgrey;">
+                  <q-item><span class="text-weight-bold">For average companies</span></q-item>
+                  <q-item>
+                      50 GB storage
+                  </q-item>
+                  <q-item>
+                      10 Domains
+                  </q-item>
+                  <q-item>
+                      15 Projects
+                  </q-item>
+                  <q-item>
+                      Free Deployment
+                  </q-item>
+                </q-list>
+              </q-card-section>
+              <q-card-section class="q-pa-none" style="color: #8b3bbd">
+                <div class="text-center">
+                  <span class="text-h2 text-center">$250</span>/mo
+                </div>
+              </q-card-section>
+              <q-card-section>
+                <div class="text-center q-px-lg">
+                  <q-btn size="md" class="full-width q-pa-xs"
+                     :style="'background: #8b3bbd; color: white'" label="Choose Plan"/>
+                     <br>
+                     <div class="q-mt-md">Terms and Conditions apply</div>
+                </div>
+              </q-card-section>
+            </q-card>
+          </div>
+          <div class="col-lg-3 col-md-3 col-xs-12 col-sm-6 q-px-lg">
+            <q-card>
+              <q-card-section class="text-white" style="background: #3b97d1">
+                <div class="text-h6 text-center">
+                  PROFESSIONAL
+                </div>
+              </q-card-section>
+              <q-card-section class="q-pa-md">
+                <q-list dense padding style="border-bottom: 1px solid lightgrey;">
+                  <q-item><span class="text-weight-bold">For large companies</span></q-item>
+                  <q-item>
+                      50 GB storage
+                  </q-item>
+                  <q-item>
+                      10 Domains
+                  </q-item>
+                  <q-item>
+                      15 Projects
+                  </q-item>
+                  <q-item>
+                      Free Deployment
+                  </q-item>
+                </q-list>
+              </q-card-section>
+              <q-card-section class="q-pa-none" style="color: #3b97d1">
+                <div class="text-center">
+                  <span class="text-h2 text-center">$500</span>/mo
+                </div>
+              </q-card-section>
+              <q-card-section>
+                <div class="text-center q-px-lg">
+                  <q-btn size="md" class="full-width q-pa-xs"
+                     style="background:#3b97d1; color: white" label="Choose Plan"/>
+                     <br>
+                     <div class="q-mt-md">Terms and Conditions apply</div>
+                </div>
+              </q-card-section>
+            </q-card>
+          </div>
+        </div>   
+        <br>
+        <br>
+        </div></div>
       </q-page>
     </q-page-container>
   </q-layout>
@@ -538,6 +711,12 @@
       font-style: italic
     .quote
       background: url(/statics/images/parallax.jpg);
+      background-size: cover;
+      background-position: center;
+      background-attachment: fixed;
+      background-repeat: no-repeat;
+    .pricing
+      background: url(/statics/images/pricing.jpg);
       background-size: cover;
       background-position: center;
       background-attachment: fixed;
