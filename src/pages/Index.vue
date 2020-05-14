5207e68cabaefe17f18d310a3b7f1ce8b1ca3d05
Mayank Patel <mayank091193@gmail.com>
2020-05-14 11:17:34 +0530

    Updated the theme. Added footer.

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -54,25 +54,31 @@
             <div class="custom-caption">
               <div class="text-h2 main_line animation_2">WELCOME TO <span :style="'color:'+theme_color">KUDOS</span></div><br><br><br>
               <div class="text-h4 animation_1">The Web as I envisaged it, we have not seen it yet. The future<br> is still so much bigger than the past.</div><br><br><br>
+<div class="animation_2">
 <q-btn size="md" :style="'background:'+ theme_color +'; color: white'" label="READ MORE"/>
+</div>
             </div>
 </div>
           </q-carousel-slide>
           <q-carousel-slide class="q-pa-none":name="2" img-src="/statics/images/image_2.jpg">
 <div class="full-height full-width flex flex-center" style="background-color: rgba(0, 0, 0, 0.68) !important;">
             <div class="custom-caption">
-    	      <div class="text-h2 main_line animation_2" style="font-size:65px;">FIRST STOP</div><br><br><br>
+    	      <div class="text-h2 main_line animation_2" style="font-size:65px;"><span :style="'color:'+theme_color">KUDOS</span> FOR THE BEST</div><br><br><br>
               <div class="text-h4 animation_1">The advance of technology is based on making it fit in so that you <br>don't really even notice it, so it's part of everyday life.</div><br><br><br>
+<div class="animation_2">
 <q-btn size="md" :style="'background:'+ theme_color +'; color: white'" label="READ MORE"/>
+</div>
             </div>
 </div>
           </q-carousel-slide>
           <q-carousel-slide class="q-pa-none":name="3" img-src="/statics/images/image_3.jpg">
 <div class="full-height full-width flex flex-center" style="background-color: rgba(0, 0, 0, 0.68) !important;">
             <div class="custom-caption">
-    	      <div class="text-h2 main_line animation_2" style="font-size:65px;">FIRST STOP</div><br><br><br>
+    	      <div class="text-h2 main_line animation_2" style="font-size:65px;">THE PATH OF <span :style="'color:'+theme_color">SUCCESS</span></div><br><br><br>
               <div class="text-h4 animation_1">We have to stop optimizing for programmers and <br>start optimizing for users.</div><br><br><br>
+<div class="animation_2">
 <q-btn size="md" :style="'background:'+ theme_color +'; color: white'" label="READ MORE"/>
+</div>
             </div>
 </div>
           </q-carousel-slide>
