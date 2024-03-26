c2bcfa73afee5cbfbe394d36353c310010307645
wl <wl674498078@gmail.com>
2024-03-26 16:51:44 +0800

    add by wl 2023/03/26 千里风光公司介绍适配

--- quasar.conf.js
+++ quasar.conf.js
@@ -84,7 +84,7 @@ module.exports = function (/* ctx */) {
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-devServer
     devServer: {
       https: false,
-      port: 8080,
+      port: 80,
       open: true // opens browser window automatically
     },
 
